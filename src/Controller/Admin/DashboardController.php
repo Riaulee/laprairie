72939a6fdbaf3858f6b1790917222c1e0889c27d
unknown <augiordano@gmail.com>
2023-09-14 16:45:07 +0200

    ajout partie du dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,11 +2,13 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -35,13 +37,24 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Laprairiesymfony')
+            ->setTitle('Administration')
             ->setLocales(['fr']);
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        if ($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToDashboard('Accueil', 'fa fa-home')->setPermission('ROLE_ADMIN');
+            yield MenuItem::section('User');
+            yield MenuItem::subMenu('User', 'fa fa-user-circle')->setSubItems([
+                MenuItem::linkToCrud('Créer un utilisateur', 'fas fa-plus-circle', User::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Liste des utilisateurs', 'fas fa-eye', User::class),
+            ]);
+            yield MenuItem::section('Post');
+            yield MenuItem::subMenu('Evènements', 'fa fa-user-circle')->setSubItems([
+                MenuItem::linkToCrud('Créer un évenement', 'fas fa-plus-circle', User::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Liste des évènements', 'fas fa-eye', User::class),
+            ]);
+        }
     }
 }

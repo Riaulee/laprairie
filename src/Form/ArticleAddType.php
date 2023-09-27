9d9313f896307532d32db38d178900742ade11f4
unknown <augiordano@gmail.com>
2023-09-27 15:19:23 +0200

    mise à jour symfony 6.2 suite

--- src/Form/ArticleAddType.php
+++ src/Form/ArticleAddType.php
@@ -7,15 +7,14 @@ use App\Entity\PostType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+// use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Validator\Constraints\Collection;
-
-// use Symfony\Component\Validator\Constraints\File;
 
 class ArticleAddType extends AbstractType
 {

b3e4dd05433ee50511b4d50d97b1b10ae16039e1
Aurelie <augiordano@gmail.com>
2024-01-06 09:31:25 +0100

    push avant release

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -12,22 +12,27 @@ class ContentController extends AbstractController
     #[Route('/quisommesnous', name: 'app_quisommesnous')]
     public function index(ContenuRepository $repo): Response
     {
-        $content1 = $repo->findOneBy(['ponderation' => 1]);
-        $content2 = $repo->findOneBy(['ponderation' => 2]);
-        $content3 = $repo->findOneBy(['ponderation' => 3]);
-        $content4 = $repo->findOneBy(['ponderation' => 4]);
-        $content5 = $repo->findOneBy(['ponderation' => 5]);
-        $content6 = $repo->findOneBy(['ponderation' => 6]);
-        $content7 = $repo->findOneBy(['ponderation' => 7]);
+        // $content1 = $repo->findOneBy(['ponderation' => 1]);
+        // $content2 = $repo->findOneBy(['ponderation' => 2]);
+        // $content3 = $repo->findOneBy(['ponderation' => 3]);
+        // $content4 = $repo->findOneBy(['ponderation' => 4]);
+        // $content5 = $repo->findOneBy(['ponderation' => 5]);
+        // $content6 = $repo->findOneBy(['ponderation' => 6]);
+        // $content7 = $repo->findOneBy(['ponderation' => 7]);
 
+        // return $this->render('pages/quisommesnous.html.twig', [
+        //     'content1' => $content1,
+        //     'content2' => $content2,
+        //     'content3' => $content3,
+        //     'content4' => $content4,
+        //     'content5' => $content5,
+        //     'content6' => $content6,
+        //     'content7' => $content7,
+        // ]);
+
+        $content = $repo->findBy([], ['ponderation' => 'asc']);
         return $this->render('pages/quisommesnous.html.twig', [
-            'content1' => $content1,
-            'content2' => $content2,
-            'content3' => $content3,
-            'content4' => $content4,
-            'content5' => $content5,
-            'content6' => $content6,
-            'content7' => $content7,
+            'content' => $content,
         ]);
     }
 }
